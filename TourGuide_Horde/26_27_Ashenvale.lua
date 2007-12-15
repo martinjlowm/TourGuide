e7f4baaa6bd7b5fbedbb5746fb56fde495746883
marekg <marekg@gmail.com>
2007-12-15 00:51:40 -0700

    User-submitted horde 23-30 updates

--- TourGuide_Horde/26_27_Ashenvale.lua
+++ TourGuide_Horde/26_27_Ashenvale.lua
@@ -5,19 +5,18 @@ h Splintertree Post
 A Stonetalon Standstill
 A Satyr Horns
 A Ashenvale Outrunners |N|Get This at the camp southwest of Splintertree Post (71,68)|
-T The Ashenvale Hunt |N|Don't skip this or the quest items won't drop!  Go to Orgrimmar and grab it if you don't have it.|
 
-K Sharptalon |L|16305| |N|Find him around south of Splintertree Post(74,71). He's level 31, so try kiting him to the guards at the nearby outpost|
-C Ashenvale Outrunners |N|Find them south of Splintertree Post, around (70,76)|
-T Ashenvale Outrunners |N|Turn this in at the camp southwest of Splintertree Post(71.1,68.2)|
+T The Ashenvale Hunt |N|Don't skip this or the quest items won't drop!  Go to Orgrimmar and grab it if you don't have it.|
+N Kill Sharptalon |N|Spawns around (74,70). Bringing him to 1/3 health than dragging him over to the guards should help.|
+C Ashenvale Outrunners |N|Go south/west around (71,72)|
+T Ashenvale Outrunners
 
 A Torek's Assault |N|Started by Torek (68,75)|
-C Torek's Assault |N|Let Torek and his bodyguard go in front of you, and join in fights that they start, otherwise they don't help you|
-C Stonetalon Standstill |N|The gazebo is the shrine on the island in the middle of Mystral Lake at (48.97,69.45)|
-K Tideress |L|16408| |N|Find him patrolling the bottom of Mystral Lake and occasionally on the island in the centre|
-K Ursangous |L|16303| |N|Find him patrolling the area northwest of Mystral Lake, around (42.1,66.2)|
-K Shadumbra |L|16304| |N|Find him patrolling the area between the path towards Astranaar and the path towards Felwood, between (56.1,57.4) and (63.1,48.6)|
-C The Sacred Flame |N|Kill the laughing sisters near the Raynewood Retreat(61,51) to get an empty vial, then use it at the moonwell at (60.20,72.90)|
+C Torek's Assault
+C Stonetalon Standstill |N|(53,70)Also find and kill Tideress he drops an item which starts The Befouled Element|
+N Kill Ursangous |N|Spawns around (42,67), for the Ashenvale Hunt|
+N Kill Shadumbra |N|Spawns around (56,54), for the Ashenvale Hunt|
+C The Sacred Flame |N|Get a phial from killing a dryad at (61,52) and fill it at the moonwell at (60,72)|
 
 H Splintertree Post
 T Stonetalon Standstill
@@ -36,11 +35,15 @@ T The Hunt Completed
 
 F Zoram'gar Outpost
 T Je'neu of the Earthen Ring
+A Vorsha the Lasher
+C Vortha the Lasher |N|On Island|
 A Between a Rock and a Thistlefur
-A Troll Charm 
+A Troll Charm
 
-C Between a Rock and a Thistlefur |N|Camp begins west of Astranaar at (31.18,45.31) and goes north north|
-C Troll Charm |N|The cave is at the back of the thistlefur camp at (38.55,30.61)|
+C Between a Rock and a Thistlefur |N|At around (34,37)|
+C Troll Charm |N|Cave is at back of the camp (38,30)|
+A Freedom to Ruul |O|Optional Escort|
+C Freedom to Ruul |N|Escort|
 T Between a Rock and a Thistlefur
 T Troll Charm
 
@@ -54,10 +57,12 @@ N Tame Ghostpaw Alpha for Bite(R4) |C|Hunter|
 N Tame Elder Ashenvale Bear for Claw(R4) |C|Hunter|
 N Retrieve Pet |C|Hunter|
 
+H Splintertree Post
+T Freedom to Ruul
 A Destroy the Legion
-C Ordanus |N|Fight to the top of the tower at (62.07,51.03). Just kill Ordanus, then loot the head and jump out of there.|
-C Satyr Horns |N|Most annoying quest ever if you can't remove curse.|
-C Destroy the Legion
+C Ordanus |N|(61,52) Fight to top room. Just kill Ordanus, loot the head then jump out of there.|
+C Satyr Horns |N|(80,52)Most annoying quest ever if you can't remove curse.|
+C Destroy the Legion |N|(82,69)Find item Diabolical Plans|
 
 R Splintertree Post |N|Don't hearth|
 T Satyr Horns
@@ -66,4 +71,3 @@ A Diabolical Plans |U|23797| |O|
 T Diabolical Plans |O|
 ]]
 end)
-
