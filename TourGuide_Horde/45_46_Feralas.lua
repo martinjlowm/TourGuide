7b4471c1c277f6d172fd0d11995c2bc7037dfd44
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-21 22:06:27 -0600

    Tag dup quests for manual QIDing

--- TourGuide_Horde/45_46_Feralas.lua
+++ TourGuide_Horde/45_46_Feralas.lua
@@ -1,30 +1,31 @@
 TourGuide:RegisterGuide("Feralas (45-46)", "Azshara (46-47)", "Horde", function()
 return [[
 F Freewind Post
-R Camp Mojache |N|West in Feralas|
-A A New Cloak's Sheen |T| |QID|2973|
-A The Ogres of Feralas (Part 1) |T| |QID|2975| 
-A Gordunni Cobalt |T| |QID|2987|
-A War on the Woodpaw |T| |QID|2862|
-A The Mark of Quality |T| |QID|2822|
-A A Strange Request |T| |QID|3121|
-h Camp Mojache |T|
+R Camp Mojache |N|South in Feralas|
+A A New Cloak's Sheen |QID|2973|
+A The Ogres of Feralas (Part 1) |QID|???|
+A Gordunni Cobalt |QID|2987|
+A War on the Woodpaw |QID|2862|
+A The Mark of Quality |QID|2822|
+A A Strange Request |QID|3121|
+h Camp Mojache
 
 C War on the Woodpaw |N|North of Camp Mojache (71,37)| |QID|2862|
-C Gordunni Cobalt |N|North of Camp Mojache, up the path by the waterfall (75,31)| |U|9466| |QID|2987|
-C The Ogres of Feralas (Part 1) |QID|2975|
-A The Gordunni Scroll |N|From the scrolls lying around on the floor| |U|9370| |QID|2978|
+C Gordunni Cobalt |N|North of Camp Mojache, up the path by the waterfall (75,31)| |QID|2987|
+C The Ogres of Feralas (Part 1) |QID|???|
+A The Gordunni Scroll |N|From the scrolls lying around on the floor| |QID|2978|
 C A New Cloak's Sheen |N|West of Camp Mojache, on the north side of the path (70,47)| |QID|2973|
 
-T War on the Woodpaw |N|Back at Camp Mojache| |T| |QID|2862|
-A Alpha Strike |T| |QID|2863|
-T Gordunni Cobalt |T| |QID|2987|
-T The Ogres of Feralas (Part 1) |T| |QID|2975|
-A The Ogres of Feralas (Part 2) |T| |QID|2980|
-T The Gordunni Scroll |T| |QID|2978|
-A Dark Ceremony |T| |QID|2979|
-T A New Cloak's Sheen |T| |QID|2973|
-A A Grim Discovery (Part 1) |T| |QID|2974|
+T War on the Woodpaw |N|Back at Camp Mojache| |QID|2862|
+T Gordunni Cobalt |QID|2987|
+T The Ogres of Feralas (Part 1) |QID|???|
+T The Gordunni Scroll |QID|2978|
+T A New Cloak's Sheen |QID|2973|
+
+A Alpha Strike |QID|2863|
+A A Grim Discovery (Part 1) |QID|???|
+A Dark Ceremony |QID|2979|
+A The Ogres of Feralas (Part 2) |QID|???|
 
 C Alpha Strike |N|Southwest of Camp Mojache (72,56)| |QID|2863|
 T Alpha Strike |T| |QID|2863|
@@ -33,55 +34,48 @@ C Woodpaw Investigation |N|Southwest of Camp Mojache (71.6,55.9)| |QID|2902|
 T Woodpaw Investigation |QID|2902|
 A The Battle Plans |QID|2903|
 
-C A Grim Discovery (Part 1) |N|West of Camp Mojache (66,46)| |QID|2974|
+C A Grim Discovery (Part 1) |N|West of Camp Mojache (66,46)| |QID|???|
 
 H Camp Mojache
-T The Battle Plans |T| |QID|2903|
-A Zukk'ash Infestation |T| |QID|7730|
-A Stinglasher |T| |QID|7731|
-T A Grim Discovery (Part 1) |T| |QID|2974|
-A A Grim Discovery (Part 2) |T| |QID|2976|
+T The Battle Plans |QID|2903|
+T A Grim Discovery (Part 1) |QID|???|
+A A Grim Discovery (Part 2) |QID|???|
+A Stinglasher |QID|7731|
+A Zukk'ash Infestation |QID|7730|
 
 C Stinglasher |N|Both in the hive area south of Camp Mojache (74,62)| |QID|7731|
 C Zukk'ash Infestation |QID|7730|
-C Screecher Spirits |N|South of Dire Maul (55,56)| |U|10699| |QID|3520|
-C The Ogres of Feralas (Part 2) |N|South of Dire Maul (59,68)| |QID|2980|
+C Screecher Spirits |N|South of Dire Maul (55,56)| |QID|3520|
+C The Ogres of Feralas (Part 2) |N|South of Dire Maul (59,68)| |QID|???|
+N Pick up a Hippograph Egg |N|Around (55,76) in nests|
 C Dark Ceremony |N|In the ruins to the south (59,68)| |QID|2979|
 N Pick up a Hippograph Egg |N|Around (55,76) in nests| |L|8564|
 C The Mark of Quality |N|Southwest of Dire Maul (55.4,56.4)| |QID|2822|
 
-T The Mark of Quality |N|Back at Camp Mojache| |T| |QID|2822|
-A The Sunken Temple |T| |QID|3445|
-T Stinglasher |T| |QID|7731|
-T Zukk'ash Infestation |T| |QID|7730|
-A Zukk'ash Report |T| |QID|7732|
-T The Ogres of Feralas (Part 2) |T| |QID|2980|
-T Dark Ceremony |T| |QID|2979|
-A The Gordunni Orb |T| |QID|3002|
+T Stinglasher |N|Back at Camp Mojache| |QID|7731|
+T Zukk'ash Infestation |QID|7730|
+T The Ogres of Feralas (Part 2) |QID|???|
+T Dark Ceremony |QID|2979|
+T The Mark of Quality |QID|2822|
+
+A Zukk'ash Report |QID|7732|
+A The Gordunni Orb |QID|3002|
+A The Sunken Temple |QID|3445|
 
 F Orgrimmar
-h Orgrimmar |T| 
-T Zukk'ash Report |N|In The Drag (56,46)| |Z|Orgrimmar| |QID|7732| |T|
-A Ripple Recovery (Part 1) |N|From Dran Droffers in The Drag (59.4,36.9)| |Z|Orgrimmar| |QID|649| |T|
-T Ripple Recovery (Part 1) |N|The other guy in the same building| |Z|Orgrimmar| |QID|649| |T|
-A Ripple Recovery (Part 2) |Z|Orgrimmar| |QID|650| |T|
-T Parts of the Swarm |N|At Belgrom Rockmaul in the Valley of Honor (75,34)| |Z|Orgrimmar| |QID|1184| |T|
-T A Grim Discovery (Part 2) |Z|Orgrimmar| |T|
-A Betrayed |QID|3507| |Z|Orgrimmar| |T|
-T A Strange Request |N|In the Cleft of Shadow (49.6,50.6)| |Z|Orgrimmar| |QID|3121| |T|
-T Report to Zor |N|In the Valley of Wisdom (38.9,38.4)| |Z|Orgrimmar| |QID|1262| |T|
-A Service to the Horde |QID|7541| |Z|Orgrimmar| |T|
-T Service to the Horde |QID|7541| |Z|Orgrimmar| |T|
-T The Gordunni Orb |N|In the Valley of Spirits (39,86)| |Z|Orgrimmar| |QID|3002| |T| 
-
-A A Donation of Silk |Z|Orgrimmar| |QID|7834| |T|
-T A Donation of Silk |Z|Orgrimmar| |QID|7834| |T|
-A A Donation of Mageweave |Z|Orgrimmar| |QID|7835| |T|
-T A Donation of Mageweave |Z|Orgrimmar| |QID|7835| |T|
-
-A A Donation of Silk |Z|Orgrimmar| |QID|7827| |T|
-T A Donation of Silk |Z|Orgrimmar| |QID|7827| |T|
-A A Donation of Mageweave |Z|Orgrimmar| |QID|7831| |T|
-T A Donation of Mageweave |Z|Orgrimmar| |QID|7831| |T|
+h Orgrimmar
+T Zukk'ash Report |N|In The Drag (56,46)| |Z|Orgrimmar| |QID|7732|
+A Ripple Recovery (Part 1) |N|From Dran Droffers in The Drag (59.4,36.9)| |Z|Orgrimmar| |QID|???|
+T Ripple Recovery (Part 1) |N|The other guy in the same building| |QID|???|
+A Ripple Recovery (Part 2) |QID|???|
+T Parts of the Swarm |N|At Belgrom Rockmaul in the Valley of Honor (75,34)| |Z|Orgrimmar| |QID|1184|
+T A Grim Discovery (Part 2) |QID|???|
+A Betrayed |QID|3507|
+T A Strange Request |N|In the Cleft of Shadow (49.6,50.6)| |Z|Orgrimmar| |QID|3121|
+T Report to Zor |N|In the Valley of Wisdom (38.9,38.4)| |Z|Orgrimmar| |QID|1262|
+A Service to the Horde |QID|7541|
+T Service to the Horde |QID|7541|
+T The Gordunni Orb |N|In the Valley of Spirits (39,86)| |Z|Orgrimmar| |QID|3002|
+N Donate cloth |N|Turn in 120 [Silk Cloth] and 120 [Mageweave Cloth] at Magar's Cloth Goods, in The Drag (63,51)| |Z|Orgrimmar|
 ]]
 end)
