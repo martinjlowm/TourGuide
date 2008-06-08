7eb365408845cc6fdabce557f89ea59ae67a923a
Tekkub <tekkub@gmail.com>
2008-06-08 15:46:32 -0600

    Fix resetting a guide not clearing QIDs

--- GuideList.lua
+++ GuideList.lua
@@ -38,6 +38,7 @@ local function OnClick(self)
     if IsShiftKeyDown() then
         TourGuide.db.char.completion[self.guide] = nil
         TourGuide.db.char.turnins[self.guide] = {}
+        for qid in string.gmatch(TourGuide.guides[self.guide](), "|QID|(%d+)|") do TourGuide.turnedinquests[tonumber(qid)] = nil end
         TourGuide:UpdateGuidesPanel()
         GameTooltip:Hide()
     else
@@ -53,63 +54,62 @@ end
 
 
 function TourGuide:CreateGuidesPanel()
-	frame = CreateFrame("Frame", nil, UIParent)
+    frame = CreateFrame("Frame", nil, UIParent)
+    rows = {}
+    for i=1,NUMROWS*3 do
+        local anchor, point = rows[i-1], "BOTTOMLEFT"
+        if i == 1 then anchor, point = frame, "TOPLEFT"
+        elseif i == (NUMROWS + 1) then anchor, point = rows[1], "TOPRIGHT"
+        elseif i == (NUMROWS*2 + 1) then anchor, point = rows[NUMROWS + 1], "TOPRIGHT" end
 
-	rows = {}
-	for i=1,NUMROWS*3 do
-		local anchor, point = rows[i-1], "BOTTOMLEFT"
-		if i == 1 then anchor, point = frame, "TOPLEFT"
-		elseif i == (NUMROWS + 1) then anchor, point = rows[1], "TOPRIGHT"
-		elseif i == (NUMROWS*2 + 1) then anchor, point = rows[NUMROWS + 1], "TOPRIGHT" end
+        local row = CreateFrame("CheckButton", nil, frame)
+        row:SetPoint("TOPLEFT", anchor, point)
+        row:SetHeight(ROWHEIGHT)
+        row:SetWidth(COLWIDTH)
 
-		local row = CreateFrame("CheckButton", nil, frame)
-		row:SetPoint("TOPLEFT", anchor, point)
-		row:SetHeight(ROWHEIGHT)
-		row:SetWidth(COLWIDTH)
+        local highlight = ww.SummonTexture(row, nil, nil, nil, "Interface\\HelpFrame\\HelpFrameButton-Highlight")
+        highlight:SetTexCoord(0, 1, 0, 0.578125)
+        highlight:SetAllPoints()
+        row:SetHighlightTexture(highlight)
+        row:SetCheckedTexture(highlight)
 
-		local highlight = ww.SummonTexture(row, nil, nil, nil, "Interface\\HelpFrame\\HelpFrameButton-Highlight")
-		highlight:SetTexCoord(0, 1, 0, 0.578125)
-		highlight:SetAllPoints()
-		row:SetHighlightTexture(highlight)
-		row:SetCheckedTexture(highlight)
+        local text = ww.SummonFontString(row, nil, "GameFontWhite", nil, "LEFT", 6, 0)
 
-		local text = ww.SummonFontString(row, nil, "GameFontWhite", nil, "LEFT", 6, 0)
+        row:SetScript("OnClick", function(...) OnClick(this) end)
+        row:SetScript("OnEnter", function(...) ShowTooltip(this) end)
+        row:SetScript("OnLeave", HideTooltip)
 
-		row:SetScript("OnClick", function(...) OnClick(this) end)
-		row:SetScript("OnEnter", function(...) ShowTooltip(this) end)
-		row:SetScript("OnLeave", HideTooltip)
+        row.text = text
+        rows[i] = row
+    end
 
-		row.text = text
-		rows[i] = row
-	end
+    frame:EnableMouseWheel()
+    frame:SetScript("OnMouseWheel", function(f, val)
+                        offset = offset - val*NUMROWS
+                        if (offset + NUMROWS*2) > table.getn(self.guidelist) then offset = offset - NUMROWS end
+                        if offset < 0 then offset = 0 end
+                        self:UpdateGuidesPanel()
+    end)
 
-	frame:EnableMouseWheel()
-	frame:SetScript("OnMouseWheel", function(f, val)
-		offset = offset - val*NUMROWS
-		if (offset + NUMROWS*2) > table.getn(self.guidelist) then offset = offset - NUMROWS end
-		if offset < 0 then offset = 0 end
-		self:UpdateGuidesPanel()
-	end)
-
-	frame:SetScript("OnShow", function(...) OnShow(this) end)
-	ww.SetFadeTime(frame, 0.5)
-	OnShow(frame)
-	return frame
+    frame:SetScript("OnShow", function(...) OnShow(this) end)
+    ww.SetFadeTime(frame, 0.5)
+    OnShow(frame)
+    return frame
 end
 
 
 function TourGuide:UpdateGuidesPanel()
-	if not frame or not frame:IsVisible() then return end
-	for i,row in ipairs(rows) do
-		row.i = i + offset + 1
+    if not frame or not frame:IsVisible() then return end
+    for i,row in ipairs(rows) do
+        row.i = i + offset + 1
 
-		local name = self.guidelist[i + offset + 1]
-		local complete = self.db.char.currentguide == name and (self.current-1)/table.getn(self.actions) or self.db.char.completion[name]
-		row.guide = name
+        local name = self.guidelist[i + offset + 1]
+        local complete = self.db.char.currentguide == name and (self.current-1)/table.getn(self.actions) or self.db.char.completion[name]
+        row.guide = name
 
-		local r,g,b = self.ColorGradient(complete or 0)
-		local text = complete and complete ~= 0 and string.format("%s |cff%02x%02x%02x[%d%%]", name, r*255, g*255, b*255, complete*100) or name
-		row.text:SetText(text)
-		row:SetChecked(self.db.char.currentguide == name)
-	end
+        local r,g,b = self.ColorGradient(complete or 0)
+        local text = complete and complete ~= 0 and string.format("%s |cff%02x%02x%02x[%d%%]", name, r*255, g*255, b*255, complete*100) or name
+        row.text:SetText(text)
+        row:SetChecked(self.db.char.currentguide == name)
+    end
 end
