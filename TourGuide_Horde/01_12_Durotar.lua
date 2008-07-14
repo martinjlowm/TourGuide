4ec8f0f381c9edbaa4e9495148b4355c74bd841e
lowlee <lowlee@lowlee.info>
2008-07-14 18:23:54 +1000

    Horde Durotar 1-12 Tweaks

--- TourGuide_Horde/01_12_Durotar.lua
+++ TourGuide_Horde/01_12_Durotar.lua
@@ -1,7 +1,7 @@
 TourGuide:RegisterGuide("Durotar (1-12)", "The Barrens (12-20)", "Horde", function()
     return [[
-A Your Place In The World |QID|4641| |NODEBUG|
-T Your Place In The World |QID|4641| |NODEBUG|
+A Your Place In The World |QID|4641|
+T Your Place In The World |QID|4641|
 A Cutting Teeth |QID|788|
 C Cutting Teeth |QID|788|
 A Sarkoth (Part 1) |N|Western Valley of Trials (40.6, 62.6)| |QID|790|
@@ -68,22 +68,27 @@ A Break a Few Eggs |T| |QID|815|
 T A Peon's Burden |T| |QID|2161|
 h Razor Hill |T|
 A Carry Your Weight |N|From watch tower northwest of town (49.9,40.3)| |QID|791|
+
 C Vanquish the Betrayers |N|Tiragarde Keep (57,55)| |QID|784|
-N If you have Benedict's Key, open the box on the ramparts upstairs from him.
+N Open the box on the ramparts upstairs from Benedict. |L|4881|
 C Carry Your Weight |QID|791|
 
 R Razor Hill
-A The Admiral's Orders |U|4883| |O| |QID|831|
+A The Admiral's Orders (Part 1) |U|4881| |QID|830|
+T The Admiral's Orders (Part 1) |QID|830|
+A The Admiral's Orders (Part 2) |QID|831|
 T Vanquish the Betrayers |QID|784|
-A From The Wreckage.... |QID|825| |NODEBUG|
+A From The Wreckage.... |QID|825|
 T Carry Your Weight |QID|791|
 C A Solvent Spirit |N|Along the beaches (62,50)| |QID|818|
-C From The Wreckage.... |N|Helps if you have water breathing| |QID|825| |NODEBUG|
-N Do the first half of "Encroachment"
+C From The Wreckage.... |N|Helps if you have water breathing| |QID|825|
+K Razormane Scout |N|Razormane Grounds (51.1,49.4)| |Q|Encroachment| |QO|Razormane Scout: 4/4|
+K Razormane Quilboar |Q|Encroachment| |QO|Razormane Quilboar: 4/4|
 
-T From The Wreckage.... |QID|825| |NODEBUG|
+T From The Wreckage.... |T| |QID|825|
+
 C Thwarting Kolkar Aggression |N|West of Sen'jin, each little camp has an item to pickup. (48,79)| |QID|786|
-C Break a Few Eggs |N|Kill Tigers if you see them.| |QID|815|
+C Break a Few Eggs |N|Head over to the Echo Isles (59.6,82.6). Kill Tigers if you see them.| |QID|815|
 C Practical Prey |QID|817|
 C Minshina's Skull |N|The skull is by the glowing ritual circle above Zalazane (67,87)| |QID|808|
 C Zalazane |N|Zalazane is at (67,86)| |QID|826|
@@ -102,8 +107,6 @@ A Winds in the Desert |N|Follow the canyon/road from Razor Hill to Orgrimmar (46
 C Winds in the Desert |QID|834|
 T Winds in the Desert |QID|834|
 A Securing the Lines |QID|835|
-C Securing the Lines |QID|835|
-T Securing the Lines |QID|835|
 
 T Encroachment |N|Back at Razor Hill| |QID|837|
 N Do Pet Quests |C|Hunter|
@@ -112,7 +115,7 @@ A Need for a Cure |N|Troll hidden in rocks to the west of Orgrimmar (41.5,18.6)|
 
 R Orgrimmar
 N Turn in Hunter Beast Quest|C|Hunter|
-T The Admiral's Orders |N|In Thrall's Room (33,37)| |QID|831|
+T The Admiral's Orders (Part 2) |N|In Thrall's Room (33,37)| |QID|831|
 A Hidden Enemies (Part 1) |QID|5726|
 A Finding the Antidote |N|In the Cleft of Shadow (46,53)| |QID|813|
 C Finding the Antidote |N|Head west towards river| |QID|813|
@@ -138,7 +141,6 @@ T Neeru Fireblade |N|Neeru Fireblade (49.6,50.4) in the Cleft of Shadow| |QID|82
 A Ak'Zeloth |QID|809|
 C Hidden Enemies (Part 2) |N|Talk to Neeru Fireblade| |QID|5727|
 A Burning Shadows |U|4903| |O| |QID|832|
-T Burning Shadows |O| |QID|832|
 T Hidden Enemies (Part 2) |N|And back to Thrall! Skip the follow-up.| |QID|5727|
 
 H Razor Hill
